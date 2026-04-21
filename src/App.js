8db698092e4008588439c6fcfd6ea597120adb13
vedjadhav2816 <vedjadhav20@gmail.com>
2026-04-21 22:22:43 +0530

    Add payment-success route to React Router
    - Added PaymentSuccess component import
    - Added /payment-success route for Stripe checkout redirect
    - Fixed navigation after successful payment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Analytics from "./pages/Analytics";
-
+import Upgrade from "./pages/Upgrade";
+import PaymentSuccess from "./pages/PaymentSuccess";
 
 
 
@@ -17,6 +18,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/analytics" element={<Analytics />} />
+          <Route path="/analytics" element={<Analytics />} />
+        <Route path="/upgrade" element={<Upgrade />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} /> 
       </Routes>
     </Router>
   );
